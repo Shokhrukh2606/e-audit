890245fc9bdf940bc8415325da055860e4657e9f
Ismoilov <di.murod.1999@gmail.com>
2020-10-21 17:30:07 +0500

    models are added

---
+++ app/Models/Use_Case.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Use_Case extends Model
+{
+    protected $table="use_cases";
+    use HasFactory;
+}
