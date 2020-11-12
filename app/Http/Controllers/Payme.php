42fa4e6d9a6f12a73874e1c56112830a97b712f7
abdumalik <axtrem13@gmail.com>
2020-11-13 04:58:56 +0500

    Test

--- app/Http/Controllers/Payme.php
+++ app/Http/Controllers/Payme.php
@@ -43,7 +43,8 @@ class Payme extends Controller
             ];
         } 
         return [
-            'result'=>$error
+            'result'=>null,
+            $error
         ];   
     }
     
