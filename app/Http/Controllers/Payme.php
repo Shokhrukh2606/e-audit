8347fa23be265da1421fafeda002ab694e2b2fda
Shokhrukh2606 <shakhrukh7ielts@mail.ru>
2020-11-13 02:10:40 +0500

    ss

--- app/Http/Controllers/Payme.php
+++ app/Http/Controllers/Payme.php
@@ -11,96 +11,115 @@ use App\Classes\PaymePaymentPaymentErrors;
 
 class Payme extends Controller
 {
-    private $click_secret_key="1HzIg1cDboO";
+    private $click_secret_key = "1HzIg1cDboO";
 
-	public function create(Request $req){
+    public function create(Request $req)
+    {
         // error checking
-        $params=$req->params;
-        $created_date=date('Y-m-d H:i:s',$params->time);
-        $transaction=Transaction::where(['id'=>$params->id, 'system_create_time'=>$created_date])->first();
-        if($transaction){
-            if($transaction->status==PaymentsStatus::WAITING){
-                $dif=time() - $transaction->system_create_time;
-                if($dif < 43200000 ){
-                    $transaction=new Transaction();
-                    $transaction->payment_system='payme';
-                    $transaction->system_transaction_id=$params->id;
-                    $transaction->invoice_id=$params->account->id;
-                    $transaction->system_create_time=$req->time;
-                    $transaction->save();
-                }else{
+        $params = $req->params;
+        $created_date = date('Y-m-d H:i:s', $params->time);
+        $transaction = Transaction::where(['id' => $params->id, 'system_create_time' => $created_date])->first();
+        if ($transaction) {
+            if ($transaction->status == PaymentsStatus::WAITING) {
+                $dif = time() - $transaction->system_create_time;
+                if ($dif < 43200000) {
                     return [
-                        'result'=>'',
-                        'error'=>-31008,
-                        'message'=>[
-                            'uz'=>'Transaction time out',
-                            'ru'=>'Transaction time out',
-                            'en'=>'Transaction time out'
+                        'create_time' => $transaction->created_at,
+                        'transaction' => $transaction->id,
+                        'state'       => $transaction->state,
+                        'receivers'   => null,
+                    ];
+                } else {
+                    return [
+                        'result' => '',
+                        'error' => [
+                            'message' => [
+                                'uz' => 'Transaction time out',
+                                'ru' => 'Transaction time out',
+                                'en' => 'Transaction time out'
+                            ],
+                            'code' => -31008
                         ]
-                    ];    
+                    ];
                 }
-            }else{
+            } else {
                 // Невозможно выполнить операцию.
                 return [
-                    'result'=>'',
-                    'error'=>-31008,
-                    'message'=>[
-                        'uz'=>'Transaction status is rejected',
-                        'ru'=>'Transaction status is rejected',
-                        'en'=>'Transaction status is rejected'
+                    'result' => '',
+                    'error' => [
+                        'code' => -31008,
+                        'message' => [
+                            'uz' => 'Transaction status is rejected',
+                            'ru' => 'Transaction status is rejected',
+                            'en' => 'Transaction status is rejected'
+                        ]
                     ]
                 ];
-
             }
-        }else{
+        } else {
+            $exist_invoice=Invoice::where([
+                'id'=>$params->account->id,
+                'status'=>'waiting',
+                'amount'=>$params->amount
+            ])->first();
+            if($exist_invoice){
+                $new_transaction=new Transaction;
+                $new_transaction->system_transaction_id=$params->id;
+                $new_transaction->system_create_time=date('Y-m-d H:i:s',$params->time);
+                $new_transaction->state=PaymentsStatus::WAITING;
+                $new_transaction->save();
+            }else{
                  return [
-                    'result'=>'',
-                    'error'=>-31008,
-                    'message'=>[
-                        'uz'=>'Transaction not found',
-                        'ru'=>'Transaction not found',
-                        'en'=>'Transaction not found'
-                    ]
-                ];
+                        'result' => '',
+                        'error' => [
+                            'message' => [
+                                'uz' => 'Transaction is not processable cause amount is not correct or id does not exist!',
+                                'ru' => 'Transaction is not processable cause amount is not correct or id does not exist!',
+                                'en' => 'Transaction is not processable cause amount is not correct or id does not exist!'
+                            ],
+                            'code' => -31008
+                        ]
+                    ];
+            }
         }
+    }
+    public function perform(Request $req)
+    {
+        // error checking
+        $error_check = new BasePaymentErrors();
+        $result = $error_check->request_check($req, $this->click_secret_key);
 
-	}
-	public function perform(Request $req){
-		// error checking
-        $error_check= new BasePaymentErrors();
-        $result=$error_check->request_check($req, $this->click_secret_key);
-        
         // if there is no error
-        if($result['error']==0){
-            $transaction=Transaction::where('id', $req->merchant_prepare_id)->first();
-            if($req->error<0){
-                if($transaction->state=='confirmed'){
+        if ($result['error'] == 0) {
+            $transaction = Transaction::where('id', $req->merchant_prepare_id)->first();
+            if ($req->error < 0) {
+                if ($transaction->state == 'confirmed') {
                     return [
                         'error' => -4,
                         'error_note' => 'Already paid'
                     ];
                 }
-                $transaction->state='rejected';
+                $transaction->state = 'rejected';
                 $transaction->save();
                 return [
                     'error' => -9,
                     'error_note' => 'Transaction cancelled'
                 ];
             }
-            $transaction->state='confirmed';
+            $transaction->state = 'confirmed';
             $transaction->save();
-            $invoice=Invoice::where('id',$req->merchant_trans_id)->first();
-            $invoice->status='confirmed';
-            $invoice->closed_with='transaction';
+            $invoice = Invoice::where('id', $req->merchant_trans_id)->first();
+            $invoice->status = 'confirmed';
+            $invoice->closed_with = 'transaction';
             $invoice->save();
-            $result+=[
-                'click_trans_id'=>$req->click_trans_id,
-                'merchant_trans_id'=>$invoice->id,
-                'merchant_confirm_id'=>$invoice->transaction->id
+            $result += [
+                'click_trans_id' => $req->click_trans_id,
+                'merchant_trans_id' => $invoice->id,
+                'merchant_confirm_id' => $invoice->transaction->id
             ];
             return $result;
-        }else{
+        } else {
             return $result;
-        }	
-	}
+        }
+    }
 }
