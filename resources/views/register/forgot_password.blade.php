beeb034f4dd8a8914d203fe89675d6dc653d35ee
Ismoilov <di.murod.1999@gmail.com>
2020-11-17 16:24:30 +0500

    minor changes

--- resources/views/register/forgot_password.blade.php
+++ resources/views/register/forgot_password.blade.php
@@ -1,9 +1,11 @@
-+998<input type="text" id="phone" placeholder="Your phone please">
++998<input type="text" id="phone" class="form-control" placeholder="Your phone please">
+<!-- <div class="form-group">
++998<input type="text" class="form-control" id="phone" placeholder="{{lang('phone')}}" />
+</div> -->
 <div class="ver_area" style="display: none">
 	<input type="text" placeholder="Your verification please" onkeyup="test_code(this)">
 </div>
 <button onclick="send_verification()">Send verification</button>
-
 
 <form action="{{route('forgot_pswrd')}}" id="pswrd_form" method="POST" style="display: none;">
 	@csrf
@@ -17,37 +19,38 @@
 <script src="{{asset('assets/js/jquery.min.js')}}"></script>
 <script src="{{asset('md5.js')}}"></script>
 <script>
-	const verification_url="{{route('verification')}}";
-	const phone_input=document.getElementById("phone");
+	const verification_url = "{{route('verification')}}";
+	const phone_input = document.getElementById("phone");
 	var verification_hash;
-	var phone="";
-	function send_verification(){
-	
-		if(phone_input.value.length!=9){
+	var phone = "";
+
+	function send_verification() {
+
+		if (phone_input.value.length != 9) {
 			alert('Please enter proper phone number');
 			return 0;
 		}
-		phone="998"+phone_input.value;
-		let url=verification_url+"?phone="+encodeURIComponent(phone);
+		phone = "998" + phone_input.value;
+		let url = verification_url + "?phone=" + encodeURIComponent(phone);
 
-		$.get(url, function(data){
-			verification_hash=data;
+		$.get(url, function(data) {
+			verification_hash = data;
 		})
 		open_verification_area();
 	}
 
-	function open_verification_area(){
-		$(".ver_area").css("display","block");
+	function open_verification_area() {
+		$(".ver_area").css("display", "block");
 	}
 
-	function test_code(elem){
-		let form=document.getElementById("pswrd_form");
-		
-		if(MD5(elem.value)==verification_hash){
+	function test_code(elem) {
+		let form = document.getElementById("pswrd_form");
+
+		if (MD5(elem.value) == verification_hash) {
 			document.getLEem
-			form.style.display="block";
-			document.getElementById("real_phone").value=phone;
-			document.getElementById("ver_code").value=verification_hash;
+			form.style.display = "block";
+			document.getElementById("real_phone").value = phone;
+			document.getElementById("ver_code").value = verification_hash;
 		}
 	}
 </script>
