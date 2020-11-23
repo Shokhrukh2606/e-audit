63061355b9fe0ca168ea0d7ab8a3fa14106fe51b
abdumalik <axtrem13@gmail.com>
2020-11-23 10:29:06 +0500

    merged

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,7 +20,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img src="{{asset('assets/img/logo.png')}}" alt="" style="width:150px">
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
