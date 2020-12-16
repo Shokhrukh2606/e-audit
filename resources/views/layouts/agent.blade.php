21bae15e6bb27efd1384317703fef1b8a63ff673
Shokhrukh2606 <shakhrukh7ielts@mail.ru>
2020-12-16 05:22:26 +0500

    info added

--- resources/views/layouts/agent.blade.php
+++ resources/views/layouts/agent.blade.php
@@ -46,10 +46,40 @@
 
         </div>
         <ul class="nav mynav">
-          <li>
+          {{-- <li>
             <a href="{{ route('agent.list_conclusions') }}">
               <i class="tim-icons icon-attach-87"></i>
               <p>{{ lang('conclusions') }}</p>
+            </a>
+          </li> --}}
+          <li>
+            <a href="{{route("auditor.list_conclusions", ['status'=>1])}}">
+              <i class="tim-icons icon-attach-87"></i>
+              <p>{{lang('initialized_conclusions')}}</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{route("auditor.list_conclusions", ['status'=>5])}}">
+              <i class="tim-icons icon-attach-87"></i>
+              <p>{{lang('received_admin_conclusions')}}</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{route("auditor.list_conclusions", ['status'=>2])}}">
+              <i class="tim-icons icon-attach-87"></i>
+              <p>{{lang('sent_conclusions')}}</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{route("auditor.list_conclusions", ['status'=>4])}}">
+              <i class="tim-icons icon-attach-87"></i>
+              <p>{{lang('rejected_conclusions')}}</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{route("auditor.list_conclusions", ['status'=>3])}}">
+              <i class="tim-icons icon-attach-87"></i>
+              <p>{{lang('finished_conclusions')}}</p>
             </a>
           </li>
           <li>
