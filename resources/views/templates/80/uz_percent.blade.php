48fd02651fc6c4289bba9f8e94e046efc013fec2
Shokhrukh2606 <shakhrukh7ielts@mail.ru>
2020-12-19 09:02:48 +0500

    ru_percent qoldi

--- resources/views/templates/80/uz_percent.blade.php
+++ resources/views/templates/80/uz_percent.blade.php
@@ -1,34 +1,115 @@
+@php
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+@endphp
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Auditor Conclusion</title>
+    <link rel="stylesheet" href="{{ asset('template/style.css') }}">
+    <style>
+        * {
+            font-family: DejaVu Sans, sans-serif;
+            font-size: 12px;
+            line-height: 13px;
+            text-align: justify;
+        }
+
+        @if ($protected)body {
+            /* background-image: url("{{ asset('shutterstock.png') }}"); */
+            background-size: 100px 100px;
+        }
+
+        @endif.qr-code {
+            position: absolute;
+            bottom: 0px;
+            left: 0;
+        }
+
+    </style>
 </head>
+
 <body>
-<p><strong>Рег.№89 23.05.2020 йил</strong></p>
-<h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size: 14pt;"> 01.05.2020 ЙИЛ ХОЛАТИ ТУЗИЛГАН МОЛИЯВИЙ ХИСОБОТЛАР БУЙИЧА&nbsp; ИҚТИСОДИЙ КУРСАТКИЧЛАРИНИ&nbsp; ТАХЛИЛ ҚИЛИШ НАТИЖАЛАРИ БЎЙИЧА ИЖОБИЙ АУДИТОРЛИК ХУЛОСАСИ.</span></h1>
-<p style="text-align: center;"><strong>&laquo;ASIA PACKS TRADING&raquo; MCHJ (STIR: 306895132)</strong></p>
-<p style="text-align: center;"><strong>&laquo;HIMOYA-AUDIT&raquo; МЧЖ Аудиторлик компанияси Сурхондарё вилояти филиали.</strong></p>
-<p>Ўзбекистон Республикаси Адлия Вазирлигидан 10.10.2007 йилда №119-сонли гувохномаси билан рўйхатдан ўтказилган. ИНН - 204107363, ОКЭД - 84400.</p>
-<p>Узбекистон Республикаси Молия Вазирлиги томонидан 05 апрел 2019 йилда берилган барча хўжалик юритувчи субъектларда аудиторлик текширувини ўтказишга рухсат берувчи АФ №00775 -сонли Лицензия.</p>
-<p>Аудитор Ш.Х.Бахромов Ўзбекистон Республикаси Молия Вазирлиги томонидан 2019&nbsp; йил 07-февраль куни берилган №005592-сонли&nbsp; аудитор малака сертификатига эга.</p>
-<p>Банк: АКБ "Агробанк" филиал Термез Р/С: 2020 8000 7041 9594 2002&nbsp; МФО: 00325.&nbsp;</p>
-<p>Ушбу махсус саволни текшириш юзасидан утказилган аудиторлик текшируви Узбекистон Республикасининг &laquo;Аудиторлик фаолияти тугрисида&raquo; ги конуни хамда 80-сонли &laquo;Махсус саволни текшириш натижалари бўйича аудитор ҳисоботи&raquo; АФМСга асосан утказилди.&nbsp;</p>
-<p><strong><u>Мазкур &laquo;ASIA PACKS TRADING&raquo; MCHJ&nbsp; </u></strong>корхонасининг 01.05.2020 йил холати бўйича амалдаги конунчиликга асосан тузилган ва аудитга такдим этилган молиявий хисоботларига асосан, 16.03.2005 йилдаги ГС-05/0271/1 сонли Қарори билан тасдиқланган, хамда Ўзбекистон Республикаси Адлия Вазирлиги томонидан 14.04.2005 йилда №1469 сон тартиб рақами билан рўйхатга олинган&nbsp; &laquo;Корхоналарнинг молиявий-иқтисодий ахволи мониторинги ва тахлилини ўтказиш мезонларини аниқлаш тартиби тўгрисида&raquo; ги Низомда кўрсатилган корхонанинг молиявий-иктисодий ахволини белгиловчи асосий кўрсаткичлари қўйидагиларни ташкил этади:</p>
-<ol>
-<li><strong> Коплаш коэффициенти=</strong> <strong>2,83</strong></li>
-</ol>
-<p style="text-align: left;">&nbsp; &nbsp; &nbsp;2. <strong>Ўз айланма маблағлари билан таъминланиш коэффициенти=</strong> <strong>0,65&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong></p>
-<ol start="3">
-<li><strong> Харажатларнинг рентабеллик коэффициенти= 0,06&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong></li>
-</ol>
-<p>Натижада <strong>01.05.2020 йил холатига корхонанинг тўлов қобилияти юқорилиги,<u> уз айланма маблаглари билан таъминланиши</u> етарли даражада булганлиги аникланди ва </strong><strong>рентабеллик коэффициенти юқори даражани ташкил этади.</strong></p>
-<p><strong>Юкоридагиларни хисобга олган холда умумий хисобда корхона иктисодий барқарор </strong><strong>хисобланади. </strong></p>
-<p><strong>Шунингдек <u>&laquo;ASIA PACKS TRADING&raquo;&nbsp; MCHJ </u></strong>корхонасининг <strong><u>01.05.2020 йил холати буйича</u></strong> тузилган молиявий хисоботи, унинг молиявий ахволини хакконий акс эттиради ва мазкур хўжалик юритувчи субъект томонидан амалга оширилган молиявий ва хўжалик операциялари Ўзбекистон Республикаси қонун хужжатларининг талабларига жавоб беради.</p>
-<p><strong>Шуни инобатга олиб корхонанинг молиявий хисоботларига ижобий хулоса берилди</strong>.</p>
-<p><strong>МЧЖ &laquo;HIMOYA-AUDIT&raquo;</strong></p>
-<p><strong>аудиторлик корхонаси&nbsp; ф/л директори:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ш.Х.Бахромов</strong></p>
-<p><strong>&nbsp;</strong></p>
-<p><strong>Аудитор:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ш.Х.Бахромов</strong></p>
-<p>&nbsp; &nbsp; <em><u>Серт</u></em><em><u>ификат&nbsp; № 05</u></em><em><u>415</u></em><em><u>. 201</u></em><em><u>6</u></em><em><u> йил 2</u></em><em><u>0-август</u></em><em><u> куни УзР Молия Вазирлиги томонидан берилган.) </u></em></p>
-<p style="text-align: right;"><strong><em><u>Такдим этилган сана : 23.05.2020 й</u></em></strong></p>
+    <div class="container">
+        <div class="uz">
+            Рег.№{{ $conclusion->id }} {{ date('d-m-Y', strtotime($conclusion->created_at)) }} йил
+            <p class="blue center bold uppercase">
+                01.05.2020 ЙИЛ ХОЛАТИ ТУЗИЛГАН МОЛИЯВИЙ ХИСОБОТЛАР БУЙИЧА ИҚТИСОДИЙ КУРСАТКИЧЛАРИНИ ТАХЛИЛ ҚИЛИШ
+                НАТИЖАЛАРИ
+                БЎЙИЧА ИЖОБИЙ АУДИТОРЛИК ХУЛОСАСИ.
+            </p>
+            <p class="bold center">{{ $conclusion->cust_info->cust_comp_name }} STIR:
+                {{ $conclusion->cust_info->cust_comp_inn }}
+            </p>
+            <p class="bold center">
+                {{ $conclusion->audit_comp_name }}.
+            </p>
+            <p>
+                Ўзбекистон Республикаси Адлия Вазирлигидан {{ $conclusion->audit_comp_gov_reg_date }} йилда
+                №{{ $conclusion->audit_comp_gov_reg_num }}-сонли гувохномаси билан рўйхатдан
+                ўтказилган. ИНН - {{ $conclusion->audit_comp_inn }}, ОКЭД - {{ $conclusion->audit_comp_oked }}.
+                Узбекистон Республикаси Молия Вазирлиги томонидан {{ $conclusion->audit_comp_lic_date }} йилда берилган
+                барча хўжалик юритувчи
+                субъектларда аудиторлик текширувини ўтказишга рухсат берувчи АФ №{{ $conclusion->audit_comp_lic }}
+                -сонли Лицензия.
+                Аудитор {{ $conclusion->auditor ? $conclusion->auditor->full_name : $conclusion->agent->full_name }}
+                Ўзбекистон Республикаси Молия Вазирлиги томонидан {{ $conclusion->cert_date }} да берилган
+                №{{ $conclusion->cert_number }}-сонли аудитор малака сертификатига эга.
+                Банк: {{ $conclusion->audit_comp_bank_name }} Р/С:
+                {{ implode(' ', str_split($conclusion->audit_comp_bank_acc, 4)) }} МФО:
+                {{ $conclusion->audit_comp_bank_mfo }}.
+                Ушбу махсус саволни текшириш юзасидан утказилган аудиторлик текшируви Узбекистон Республикасининг
+                «Аудиторлик фаолияти тугрисида» ги конуни хамда 80-сонли «Махсус саволни текшириш натижалари бўйича
+                аудитор
+                ҳисоботи» АФМСга асосан утказилди.
+            </p>
+            <p>
+                <span class="underline bold">
+                    Мазкур «ASIA PACKS TRADING» MCHJ </span>корхонасининг 01.05.2020 йил холати бўйича амалдаги
+                конунчиликга асосан
+                тузилган ва аудитга такдим этилган молиявий хисоботларига асосан, 16.03.2005 йилдаги ГС-05/0271/1 сонли
+                Қарори билан тасдиқланган, хамда Ўзбекистон Республикаси Адлия Вазирлиги томонидан 14.04.2005 йилда
+                №1469
+                сон тартиб рақами билан рўйхатга олинган «Корхоналарнинг молиявий-иқтисодий ахволи мониторинги ва
+                тахлилини
+                ўтказиш мезонларини аниқлаш тартиби тўгрисида» ги Низомда кўрсатилган корхонанинг молиявий-иктисодий
+                ахволини белгиловчи асосий кўрсаткичлари қўйидагиларни ташкил этади:
+            </p>
+            <p class="bold">
+                1. Коплаш коэффициенти= {{ $conclusion->kps()['value'] }}<br>
+                2.Ўз айланма маблағлари билан таъминланиш коэффициенти= {{ $conclusion->koses()['value'] }}<br>
+                3. Харажатларнинг рентабеллик коэффициенти= {{ $conclusion->kpp()['value'] }}<br>
+            </p>
+            <p>
+                Натижада
+                <span> 01.05.2020 йил {{ $conclusion->conclusion_base }}
+                </span>
+            </p>
+            Шунингдек {{ $conclusion->cust_info->cust_comp_name }} корхонасининг 01.05.2020 йил холати буйича тузилган
+            молиявий хисоботи,
+            унинг молиявий ахволини хакконий акс эттиради ва мазкур хўжалик юритувчи субъект томонидан амалга оширилган
+            молиявий ва хўжалик операциялари Ўзбекистон Республикаси қонун хужжатларининг талабларига жавоб беради.
+            Шуни инобатга олиб корхонанинг молиявий хисоботларига ижобий хулоса берилди.
+            <p class="bold">
+                {{ $conclusion->audit_comp_name }} директори:&nbsp;&nbsp;&nbsp;&nbsp;
+                {{ $conclusion->audit_comp_director_name }}
+            </p>
+            <p class="bold">
+                Аудитор: <span
+                    class="space-needed">{{ $conclusion->auditor ? $conclusion->auditor->full_name : $conclusion->agent->full_name }}</span>
+            </p>
+            <p class="text-left">
+            <div class="qr-code">
+                <img src="data:application/octet-stream;base64, {{ $qrcode }}" alt="">
+            </div>
+            </p>Сертификат
+            №{{ $conclusion->auditor ? $conclusion->auditor->cert_number : $conclusion->agent->cert_number }}.
+            {{ date('d-m-Y', strtotime($conclusion->cert_date)) }} да УзР Молия Вазирлиги томонидан берилган.)
+        </div>
+    </div>
 </body>
+
 </html>
